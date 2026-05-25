77162804e3a452900013cf209ad083517ef7f4d7
Nguyễn Duy Mạnh <style@MacBook-Pro-cua-Nguyen.local>
2026-05-25 19:56:52 +0700

    debug(momo): log ipnUrl and endpoint

--- services/payment/providers/momoProvider.js
+++ services/payment/providers/momoProvider.js
@@ -115,6 +115,8 @@ async function createPayment({
 
   };
 
+  console.log("[MOMO] ipnUrl:", MOMO_CONFIG.ipnUrl);
+  console.log("[MOMO] endpoint:", MOMO_CONFIG.endpoint);
   const response =
     await axios.post(
       MOMO_CONFIG.endpoint,
